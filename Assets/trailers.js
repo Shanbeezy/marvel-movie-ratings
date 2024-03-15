9c3676d2e53c7649d34657348e9cfd901f86b6c6
Shannon James Jr. <jay.are.sj@gmail.com>
2024-03-15 19:16:12 -0400

    changed js filename to trailers.js and added carlos updated function

--- Assets/trailers.js
+++ Assets/trailers.js
@@ -29,13 +29,12 @@
 // video Ids********************
 
 
-
-
 const apiKey = 'AIzaSyB_Q_N5U2Hnsr2IEO-TbhaG3or0ya7nJao';
 const videoIds = ['8ugaeA-nMTc','xbqNb2PFKKA','wKtcmiifycU','JOddp-nlNvQ','JerVrbLldXw',
 'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
 'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
 '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE'];
+
 async function fetchVideoByIds() {
     try {
         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
@@ -48,19 +47,15 @@ async function fetchVideoByIds() {
 
 function renderVideo(video) {
     const videoContainer = document.getElementById('video');
-    const videoElement = document.createElement('iframe');
-    videoElement.src = `https://www.youtube.com/embed/${video.id}`;
-    videoElement.width = '560';
-    videoElement.height = '315';
-    videoElement.title = video.snippet.title;
-    videoElement.frameBorder = '0';
-    videoContainer.appendChild(videoElement);
+    videoContainer.src = `https://www.youtube.com/embed/${video.id}`;
 }
 
-fetchVideoByIds();
-
-
-
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    const videoButtons = document.querySelectorAll('.video-button');
+    videoButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const videoId = this.getAttribute('data-video-id');
+            fetchVideoById(videoId);
+        });
+    });
+});
