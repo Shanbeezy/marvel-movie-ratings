356be55707f4e3a4943fb4a1f04518327769ec83
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-03-15 18:39:31 -0400

    fixed function to work on click of each button element

--- Assets/script.js
+++ Assets/script.js
@@ -29,37 +29,37 @@
 // video Ids********************
 
 
-
-
 const apiKey = 'AIzaSyB_Q_N5U2Hnsr2IEO-TbhaG3or0ya7nJao';
 const videoIds = ['8ugaeA-nMTc','xbqNb2PFKKA','wKtcmiifycU','JOddp-nlNvQ','JerVrbLldXw',
-'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
-'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
-'8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE'];
-async function fetchVideoByIds() {
-    try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
-        const data = await response.json();
-        renderVideo(data.items[0]);
-    } catch (error) {
-        console.error('Error fetching video:', error);
-    }
-}
-
-function renderVideo(video) {
-    const videoContainer = document.getElementById('video');
-    const videoElement = document.createElement('iframe');
-    videoElement.src = `https://www.youtube.com/embed/${video.id}`;
-    videoElement.width = '560';
-    videoElement.height = '315';
-    videoElement.title = video.snippet.title;
-    videoElement.frameBorder = '0';
-    videoContainer.appendChild(videoElement);
-}
-
-fetchVideoByIds();
-
-
+ 'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
+ 'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
+ '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE']; // Array of video IDs
+ 
+ async function fetchVideoById(videoId) {
+     try {
+         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
+         const data = await response.json();
+         renderVideo(data.items[0]);
+     } catch (error) {
+         console.error('Error fetching video:', error);
+     }
+ }
+ 
+ function renderVideo(video) {
+     const videoContainer = document.getElementById('video');
+     videoContainer.src = `https://www.youtube.com/embed/${video.id}`;
+ }
+ 
+ document.addEventListener('DOMContentLoaded', () => {
+     const videoButtons = document.querySelectorAll('.video-button');
+     videoButtons.forEach(button => {
+         button.addEventListener('click', function() {
+             const videoId = this.getAttribute('data-video-id');
+             fetchVideoById(videoId);
+         });
+     });
+ });
+ 
 
 
 
