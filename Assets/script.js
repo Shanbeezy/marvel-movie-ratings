af8706cb55a88021384053f33301cbd76241c538
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-03-14 20:44:55 -0400

    added more to js

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@
 // one for youtube videos that respond to search, one for youtube videos on load
 // 4 fetches
 
-// Video Ids: 
+// Video Ids*****************
 // Iron man 1 : 8ugaeA-nMTc
 // The Incredible Hulk : xbqNb2PFKKA
 // Iron man 2 : wKtcmiifycU 
@@ -18,14 +18,23 @@
 // Captain America Civil War : FkTybqcX-Yo
 // Doctor Strange : h7gvFravm4A
 // Guardians of the Galaxy Vol .2 : dW1BIid8Osg
+// Spider-man Homecoming : rk-dF1lIbIg
+// Thor Ragnarok : v7MGUNV8MxU
+// Black Panther : xjDjIWPwcPU
+// Avengers Ininifty War : 6ZfuNTqbHE8
+// Antman and the Wasp : 8_rTIAOohas
+// Captain Marvel : Z1BCujX3pw8
+// Avengers Endgame : TcMBFSGVi1c
+// Spider-man Far From Home : Nt9L1jCKGnE
+// video Ids********************
 
 
 
 
 const apiKey = 'AIzaSyB_Q_N5U2Hnsr2IEO-TbhaG3or0ya7nJao';
-const videoId = '8ugaeA-nMTc';
+const videoIds = '8ugaeA-nMTc';
 
-async function fetchVideoById() {
+async function fetchVideoByIds() {
     try {
         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
         const data = await response.json();
@@ -46,7 +55,7 @@ function renderVideo(video) {
     videoContainer.appendChild(videoElement);
 }
 
-fetchVideoById();
+fetchVideoByIds();
 
 
 
