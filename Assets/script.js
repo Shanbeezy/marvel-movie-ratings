e7781a341460b905151808b63234decd8e973761
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-03-18 14:23:55 -0400

    got first video to pop up on click

--- Assets/script.js
+++ Assets/script.js
@@ -41,39 +41,79 @@ function getApi() {
 // Avengers Endgame : TcMBFSGVi1c
 // Spider-man Far From Home : Nt9L1jCKGnE
 // video Ids********************
+ 
+  
+//  const apiKey = 'AIzaSyB_Q_N5U2Hnsr2IEO-TbhaG3or0ya7nJao';
+//  const videoIds = ['8ugaeA-nMTc','xbqNb2PFKKA','wKtcmiifycU','JOddp-nlNvQ','JerVrbLldXw',
+//   'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
+//   'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
+//   '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE']; // Array of video IDs
+
+// async function fetchVideoById(videoId) {
+//     try {
+//         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
+//         const data = await response.json();
+//         const videoEmbedUrl = `https://www.youtube.com/embed/${videoId}`;
+//         renderVideo(videoEmbedUrl);
+//     } catch (error) {
+//         console.error('Error fetching video:', error);
+//     }
+// }
+
+// function renderVideo(videoEmbedUrl) {
+//     const videoContainer = document.getElementById('video-container');
+//     videoContainer.innerHTML = `<iframe width="560" height="315" src="${videoEmbedUrl}" frameborder="0" allowfullscreen></iframe>`;
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const imgElements = document.querySelectorAll('img#thumbnail');
+//     imgElements.forEach(img => {
+//         img.addEventListener('click', function() {
+//             const videoId = this.getAttribute('data-video-id');
+//             fetchVideoById(videoId);
+//         });
+//     });
+// });
 
 
 const apiKey = 'AIzaSyB_Q_N5U2Hnsr2IEO-TbhaG3or0ya7nJao';
 const videoIds = ['8ugaeA-nMTc','xbqNb2PFKKA','wKtcmiifycU','JOddp-nlNvQ','JerVrbLldXw',
- 'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
- 'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
- '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE']; // Array of video IDs
- 
- async function fetchVideoById(videoId) {
-     try {
-         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
-         const data = await response.json();
-         renderVideo(data.items[0]);
-     } catch (error) {
-         console.error('Error fetching video:', error);
-     }
- }
- 
- function renderVideo(video) {
-     const videoContainer = document.getElementById('video');
-     videoContainer.src = `https://www.youtube.com/embed/${video.id}`;
- }
- 
- document.addEventListener('DOMContentLoaded', () => {
-     const videoButtons = document.querySelectorAll('.video-button');
-     videoButtons.forEach(button => {
-         button.addEventListener('click', function() {
-             const videoId = this.getAttribute('data-video-id');
-             fetchVideoById(videoId);
-         });
-     });
- });
- 
+  'eOrNdBpGMv8','oYSD2VQagc4','npvJ9FTgZbM','tbayiPxkUMM','d96cjJhvlMA','tmeOjFno6Do','pWdKf3MneyI',
+  'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
+  '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE']; // Array of video IDs
+
+async function fetchVideoById(videoId) {
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
+        const data = await response.json();
+        const videoEmbedUrl = `https://www.youtube.com/embed/${videoId}`;
+        renderVideo(videoEmbedUrl);
+
+        // Scroll to the video container
+        const videoContainer = document.getElementById('video-container');
+        videoContainer.scrollIntoView({ behavior: 'instant' });
+    } catch (error) {
+        console.error('Error fetching video:', error);
+    }
+}
+
+function renderVideo(videoEmbedUrl) {
+    const videoContainer = document.getElementById('video-container');
+    videoContainer.innerHTML = `<iframe width="560" height="315" src="${videoEmbedUrl}" frameborder="0" allowfullscreen></iframe>`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgElements = document.querySelectorAll('img#thumbnail');
+    imgElements.forEach(img => {
+        img.addEventListener('click', function() {
+            const videoId = this.getAttribute('data-video-id');
+            fetchVideoById(videoId);
+        });
+    });
+});
+
+
+
 
 
 
