b81894afb5e7fc7a7c790119be601f05277518f7
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-03-19 19:34:43 -0400

    added a simple modal and deleted two useless files

--- Assets/script.js
+++ Assets/script.js
@@ -30,7 +30,7 @@ const videoIds = ['8ugaeA-nMTc','xbqNb2PFKKA','wKtcmiifycU','JOddp-nlNvQ','JerVr
   'FkTybqcX-Yo','h7gvFravm4A','dW1BIid8Osg','rk-dF1lIbIg','v7MGUNV8MxU','xjDjIWPwcPU','6ZfuNTqbHE8',
   '8_rTIAOohas','Z1BCujX3pw8','TcMBFSGVi1c','Nt9L1jCKGnE']; // Array of video IDs
 
-// Function to 
+// Function to fetch youtube trailers using API key 
 async function fetchVideoById(videoId) {
     try {
         const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
@@ -78,8 +78,13 @@ function closeVideoContainer() {
     videoContainer.classList.remove('open'); // Hide the video container
 }
 
-
-
+Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "You have now entered the Marvel Universe",
+    showConfirmButton: false,
+    timer: 1500
+  });
 
 
 
